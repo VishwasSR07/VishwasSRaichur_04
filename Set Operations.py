121b0223ebbce7a38ccf67568d7552e7f79eafea
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 12:22:03 +0530

    Create  Set Operations.py

---
+++ Set Operations.py
@@ -0,0 +1,14 @@
+# 22. Set Operations
+set1, set2 = {1, 2, 3}, {3, 4, 5}
+print(set1 | set2, set1 & set2, set1 - set2)
+
+# 23. Add/Remove Elements in Set
+set1.add(6)
+set1.remove(2)
+print(set1)
+
+# 24. Check Element in Set
+print(3 in set1)
+
+# 25. Common Elements in Two Sets
+print(set1 & set2)
